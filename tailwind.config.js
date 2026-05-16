9f2e2d6e79bdb583a8a1f8901700ffabd56b56ca
Azeem Khalid <azeemkforyou@gmail.com>
2026-05-16 14:59:36 -0700

    add plus jakarta sans UI update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['"Plus Jakarta Sans"', 'sans-serif'],
+      },
       colors: {
         primary: {
           50: '#f0fdfa',
